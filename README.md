4955defb2d60726055ce370f0c1f65bdb36e1754
diegobalestieri <diego.balestieri1998@gmail.com>
2021-05-18 19:53:27 -0300

    agrego primera funcion que llama a la API de proyectos y pasa esa info

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Gateway
+Cuidado con la URL, tuve que poner la de mi PC para que funcione. Asi que si tira un error de tipo 500 correr ifconfig en la terminal y poner ese ip en la URL
--- prod/api/projects_list_api.py
+++ prod/api/projects_list_api.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, request
 from flask_restful import Api, Resource
+import requests
+URL = 'http://172.22.0.1:5000/projects'
 
 projects_list_api = Blueprint("projects_list_api", __name__)
 api = Api(projects_list_api)
 
-
 class ProjectsListResource(Resource):
     def get(self):
-        return "hello"
+        response = requests.get(URL)
+        return response.json()
+
+    def post(self):
+        response = requests.post(URL, json=request.get_json())
+        return response.json()
 
 
 api.add_resource(ProjectsListResource, "/projects")
