458f2c2dc8ea2fcaaf867db845393973b5fbf6f8
diegobalestieri <diego.balestieri1998@gmail.com>
2021-06-05 21:03:03 -0300

    agrego endpoint con post y get de proyectos para los usuarios

--- prod/api/Projects/Mobile/my_projects_list.py
+++ prod/api/Projects/Mobile/my_projects_list.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from flask_restful import Api, Resource
 import requests
-URL_PROJETCS = 'https://seedy-fiuba-backend-projects.herokuapp.com/projects/'
+URL_PROJETCS = 'https://seedy-fiuba-backend-projects.herokuapp.com/projects'
 URL_USERS = 'https://seedy-fiuba-backend-users.herokuapp.com/users/'
 
 my_projects_list_api = Blueprint("my_projects_list_api", __name__)
@@ -9,13 +9,18 @@ api = Api(my_projects_list_api)
 
 class MyProjectsListResource(Resource):
     def get(self, user_id):
-        response = requests.get(URL_USERS+user_id)
+        response = requests.get(URL_USERS+user_id+'/projects')
+        projects_list = response.json()['project_id']
+        return_value = []
+        for project in projects_list:
+            return_value.append(requests.get(URL_PROJETCS+'/'+str(project)).json())
+        return return_value
+
+    def post(self, user_id):
+        response = requests.post(URL_PROJETCS, json=request.get_json())
+        if response.status_code == 201:
+            requests.post(URL_USERS+user_id+'/projects', json={"project_id": response.json()['id']})
         return response.json()
 
-'''
-    def post(self, user_id):
-        response = requests.post(URL, json=request.get_json())
-        return response.json()
-'''
 
 api.add_resource(MyProjectsListResource, "/users/<user_id>/projects")
