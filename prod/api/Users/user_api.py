8add2b0ccc247e2c72cb8e120a5cbcdeca0de6b9
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-06-09 11:10:21 -0300

    feat: patch users/id => Actualiza perfil.

--- prod/api/Users/user_api.py
+++ prod/api/Users/user_api.py
@@ -1,7 +1,8 @@
-from flask import request
 from flask_restx import Namespace, Resource, fields
+from flask import request
 import requests
 import os
+
 URL_USERS = os.getenv("USERS_BACKEND_URL") + "/users/"
 
 ns = Namespace(
@@ -14,6 +15,14 @@ ns = Namespace(
 @ns.param('user_id', 'The user identifier')
 class UserResource(Resource):
     USER_NOT_EXIST_ERROR = 'This user does not exists'
+    REPEATED_EMAIL_ERROR = 'repeated_email'
+
+    body_swg = ns.model('NotRequiredUserInput', {
+        "name": fields.String(description="The user new name"),
+        "lastName": fields.String(description="The user new last name"),
+        "email": fields.String(description="The user new email"),
+        "password": fields.String(description="The user new password")
+    })
 
     code_200_swg = ns.model('UserOutput200', {
         "id": fields.Integer(description='The user id'),
@@ -27,8 +36,22 @@ class UserResource(Resource):
         'status': fields.String(example=USER_NOT_EXIST_ERROR)
     })
 
-    @ns.marshal_with(code_200_swg, code=200)
+    code_409_swg = ns.model('UserOutput409', {
+        'status': fields.String(example=REPEATED_EMAIL_ERROR)
+    })
+
+    @ns.response(200, 'Success', code_200_swg)
     @ns.response(404, USER_NOT_EXIST_ERROR, code_404_swg)
     def get(self, user_id):
+        """Get user data"""
         response = requests.get(URL_USERS+user_id)
+        return response.json()
+
+    @ns.expect(body_swg)
+    @ns.response(200, 'Success', code_200_swg)
+    @ns.response(404, USER_NOT_EXIST_ERROR, code_404_swg)
+    @ns.response(409, REPEATED_EMAIL_ERROR, code_409_swg)
+    def patch(self, user_id):
+        """Update user data"""
+        response = requests.patch(URL_USERS+user_id, json=request.get_json())
         return response.json()
