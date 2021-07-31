b42e4f3eb9955a3e566dae1d839971ca55879953
Yuquimura <brianzambellitello@gmail.com>
2021-07-31 01:41:25 -0300

    New: Documentacion correcta de project_fund

--- prod/api/Projects/Mobile/project_fund_api.py
+++ prod/api/Projects/Mobile/project_fund_api.py
@@ -3,45 +3,23 @@ from flask_restx import Namespace, Resource, fields
 import requests
 import os
 from prod import api_error_handler
+from prod.schemas.project_fund_api import ns
+from prod.schemas.common.found_schema import body_swg
+from prod.schemas.project_fund_api import get_models
 
 PAYMENTS_API_KEY = os.getenv("PAYMENTS_API_KEY")
 URL_USERS = os.getenv("USERS_BACKEND_URL")
 URL_PAYMENTS = os.getenv("PAYMENTS_BACKEND_URL") + "/projects/"
 
-ns = Namespace(
-    'projects/<string:project_id>/funds',
-    description='Project funds related operations'
-)
 
 @ns.route('')
 @ns.param('project_id', 'The project identifier')
 class ProjectResource(Resource):
-    SUCCESS = 'Transaction being mined'
-    PROJECT_NOT_FOUND_ERROR = 'The project requested could not be found'
-    SERVER_ERROR = "503 Server Error: Service Unavailable for url"
-    body_swg = ns.model('Project_Fund_Payload', {
-        'userPublicId': fields.Integer(description='The user id who wants to fund'),
-        'amountEthers': fields.String(description='The amount of ethers to fund')
-    })
-    code_202_swg = ns.model('Project_Funded_Success', {
-        'id': fields.Integer(description='The transaction Id'),
-        'amountEthers': fields.String(description='The amount of ethers fund'),
-        'fromPublicId': fields.String(description='The id of the user funder'),
-        'fromType': fields.String(example='user'),
-        'toPublicId': fields.String(description='The id of the project being fund'),
-        'toType': fields.String(example='project'),
-        'transactionType': fields.String(example='fund'),
-        'transationState': fields.String(example='mining / done'),
-        'token': fields.String(description='Updated token')
-    })
-    code_503_swg = ns.model('ProjectOutput503', {
-        'status': fields.String(example=SERVER_ERROR)
-    })
 
     @ns.doc(params={'token': {'in': 'query', 'type': 'string'}})
     @ns.expect(body_swg)
-    @ns.response(202, SUCCESS, code_202_swg)
-    @ns.response(503, SERVER_ERROR, code_503_swg)
+    @ns.response(202, get_models['202'][0], get_models['202'][1])
+    @ns.response(503, get_models['503'][0], get_models['503'][1])
     def post(self, project_id):
         first_data = request.get_json()
         token = request.args.get('token')
@@ -53,7 +31,7 @@ class ProjectResource(Resource):
         if status_code != 200:
             return response_object, status_code
         new_token = response_object['token']
-        url = URL_PAYMENTS+project_id+'/funds'
+        url = URL_PAYMENTS + project_id + '/funds'
         response = requests.post(
             url,
             headers={"Authorization": PAYMENTS_API_KEY},
