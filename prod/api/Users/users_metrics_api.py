0dffe6f0d10e5b525a1d5f7a1aa95917ebdde06a
Yuquimura <brianzambellitello@gmail.com>
2021-07-31 19:22:08 -0300

    Fix: Documentacion de users_metrics_api.py

--- prod/api/Users/users_metrics_api.py
+++ prod/api/Users/users_metrics_api.py
@@ -2,38 +2,16 @@ from flask_restx import Namespace, Resource, fields
 import requests
 import os
 from prod import api_error_handler
-URL_USERS = os.getenv("USERS_BACKEND_URL") + "/users/metrics"
+from prod.schemas.login_api_schema import ns, get_models
 
-ns = Namespace(
-    name='users/metrics',
-    description='All users related operations'
-)
+URL_USERS = os.getenv("USERS_BACKEND_URL") + "/users/metrics"
 
 
 @ns.route('')
 class UsersListResource(Resource):
-    MISSING_VALUES_ERROR = 'missing_args'
-    REPEATED_USER_ERROR = 'repeated_email'
-    SERVER_ERROR = "503 Server Error: Service Unavailable for url"
 
-    body_swg = ns.model('One user input', {
-        "name": fields.String(required=True, description="The user name"),
-        "lastName": fields.String(
-            required=True, description="The user last name"),
-        "email": fields.String(required=True, description="The user email"),
-        "active": fields.Boolean(required=True, description="The user status")
-    })
-    code_20x_swg = ns.model('One user output 20x', {
-        "percentage_blocked": fields.Integer(description='The percentage of users blocked'),
-        "percentage_with_project": fields.Integer(description="The percentage of users with projects"),
-        "percentage_seer": fields.Integer(description="The percentage of users that are seers")
-    })
-    code_503_swg = ns.model('UserMetricsOutput503', {
-        'status': fields.String(example=SERVER_ERROR)
-    })
-
-    @ns.response(200, 'Success', fields.List(fields.Nested(code_20x_swg)))
-    @ns.response(503, SERVER_ERROR, code_503_swg)
+    @ns.response(200, get_models['200'][0], get_models['200'][1])
+    @ns.response(503, get_models['503'][0], get_models['503'][1])
     def get(self):
         """Get all users data"""
         response = requests.get(URL_USERS)
