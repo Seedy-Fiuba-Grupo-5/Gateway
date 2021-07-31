7943092be91f3d71c819f30b6f879e8753155d85
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-07-31 11:51:30 -0300

    add: test for patch project

--- dev/tests/test_project_api.py
+++ dev/tests/test_project_api.py
@@ -35,3 +35,29 @@ def test_get_project(test_app, requests_mock):
     body = json.loads(response.data.decode())
     assert body['user'] == json_user
     assert body['payments'] == json_pay
+
+def test_patch_project(test_app, requests_mock):
+    project_id = 1
+    user_id = 1
+    token = "a token"
+    path_client = "/projects/" + str(project_id)
+    data_client = { "token": token }
+
+    url_user_project = USERS_BACKEND_URL + "/projects/" + str(project_id)
+    json_user_project = { "user_id": user_id }
+    requests_mock.get(url_user_project, status_code=200, json=json_user_project)
+
+    url_auth = USERS_BACKEND_URL + "/users/auth"
+    json_auth = {}
+    requests_mock.post(url_auth, status_code=200, json=json_auth)
+
+    url_project = PROJECTS_BACKEND_URL + "/projects/" + str(project_id)
+    json_project = {}
+    requests_mock.patch(url_project, status_code=200, json=json_project)
+
+    client = test_app.test_client()
+    response = client.patch(path_client, data=json.dumps(data_client), content_type="application/json")
+    assert response.status_code == 200
+    body = json.loads(response.data.decode())
+    assert body == json_project
+
