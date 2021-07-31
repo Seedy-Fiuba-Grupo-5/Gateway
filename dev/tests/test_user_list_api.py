ff63592b61a4976d3a420697ee93ec6aec6eb585
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-07-31 16:13:24 -0300

    add: test to get users list

---
+++ dev/tests/test_user_list_api.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+PROJECTS_BACKEND_URL = os.getenv('PROJECTS_BACKEND_URL')
+USERS_BACKEND_URL = os.getenv('USERS_BACKEND_URL')
+PAYMENTS_BACKEND_URL = os.getenv('PAYMENTS_BACKEND_URL')
+
+def get_user_list(test_app, requests_mock):
+    path_client = "/users"
+
+    url_users_list = USERS_BACKEND_URL + "/users"
+    json_users_list = [{"id": 1}, {"id": 2}]
+    requests_mock.get(url_users_list, status_code=200, json=json_users_list)
+
+    client = test_app.test_client()
+    response = client.get(path_client)
+    assert response.status_code == 200
+    body = json.loads(response.data.decode())
+    assert body == json_users_list
