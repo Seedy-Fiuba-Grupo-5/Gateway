c107ee611e0605cf2d21c6a8980a5e2fc4ac62ff
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-07-31 02:42:58 -0300

    add: test for post rating

--- dev/tests/test_ratings_api.py
+++ dev/tests/test_ratings_api.py
@@ -19,3 +19,18 @@ def test_get_ratings(test_app, requests_mock):
     assert response.status_code == 200
     body = json.loads(response.data.decode())
     assert body == body_client
+
+def test_post_ratings(test_app, requests_mock):
+    project_id = 1
+    path_client = "/projects/" + str(project_id) + "/rate"
+    data_client = {}
+    body_client = {}
+
+    url_rate = PROJECTS_BACKEND_URL + "/projects/" + str(project_id) + "/rate"
+    requests_mock.post(url_rate, status_code=200, json=body_client)
+
+    client = test_app.test_client()
+    response = client.post(path_client, data=json.dumps(data_client), content_type="application/json")
+    assert response.status_code == 200
+    body = json.loads(response.data.decode())
+    assert body == body_client
