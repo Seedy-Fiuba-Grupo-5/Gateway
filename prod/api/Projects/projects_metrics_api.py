b5ba5a93b26dd140065da02254f1314d19e8d39e
Yuquimura <brianzambellitello@gmail.com>
2021-07-31 19:00:16 -0300

    Fix: Documentacion de projects_metrics_api.py

--- prod/api/Projects/projects_metrics_api.py
+++ prod/api/Projects/projects_metrics_api.py
@@ -2,31 +2,16 @@ from flask_restx import Namespace, Resource, fields
 import requests
 import os
 from prod import api_error_handler
-URL_PROJECTS = os.getenv("PROJECTS_BACKEND_URL") + "/projects/metrics"
+from prod.schemas.metrics_api_schema import ns, get_models
 
-ns = Namespace(
-    name='projects/metrics',
-    description='All projects metrics'
-)
+URL_PROJECTS = os.getenv("PROJECTS_BACKEND_URL") + "/projects/metrics"
 
 
 @ns.route('')
 class UsersListResource(Resource):
-    MISSING_VALUES_ERROR = 'missing_args'
-    REPEATED_USER_ERROR = 'repeated_email'
-    SERVER_ERROR = "503 Server Error: Service Unavailable for url"
 
-    code_20x_swg = ns.model('Project metrics output 20x', {
-        "most_popular_type": fields.Integer(description='Most popular type of project'),
-        "avg_goal": fields.Integer(description="Average project goal"),
-        "avg_duration": fields.Integer(description="Average project duration in months")
-    })
-    code_503_swg = ns.model('ProjectMetricsOutput503', {
-        'status': fields.String(example=SERVER_ERROR)
-    })
-
-    @ns.response(200, 'Success', fields.List(fields.Nested(code_20x_swg)))
-    @ns.response(503, SERVER_ERROR, code_503_swg)
+    @ns.response(200, get_models['200'][0], get_models['200'][1])
+    @ns.response(503, get_models['503'][0], get_models['503'][1])
     def get(self):
         """Get all users data"""
         response = requests.get(URL_PROJECTS)
