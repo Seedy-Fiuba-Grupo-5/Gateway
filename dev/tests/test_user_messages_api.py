fa68f209517dc5b98f461420c3dcbad88da1c375
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-07-31 16:37:12 -0300

    add: test to post user message

--- dev/tests/test_user_messages_api.py
+++ dev/tests/test_user_messages_api.py
@@ -20,4 +20,20 @@ def test_get_user_messages(test_app, requests_mock):
     body = json.loads(response.data.decode())
     assert body == json_messages
 
+def test_post_user_message(test_app, requests_mock):
+    user_id = 1
+    other_id = 2
+    path_client = "/messages/" + str(user_id)
+    data_client = {}
+
+    url_messages = USERS_BACKEND_URL + "/messages/" + str(user_id)
+    json_messages = {"id_1": other_id}
+    requests_mock.post(url_messages, status_code=201, json=json_messages)
+
+    client = test_app.test_client()
+    response = client.post(path_client, data=json.dumps(data_client), content_type='application/json')
+    assert response.status_code == 201
+    body = json.loads(response.data.decode())
+    assert body == json_messages
+
 
