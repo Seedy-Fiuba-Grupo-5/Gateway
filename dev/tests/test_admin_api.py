404a6e6baca573d350f55ac0a3d51219eb378d80
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-08-01 02:54:20 -0300

    add: test to patch admin

--- dev/tests/test_admin_api.py
+++ dev/tests/test_admin_api.py
@@ -18,3 +18,20 @@ def test_get_admin(test_app, requests_mock):
     assert response.status_code == 200
     body = json.loads(response.data.decode())
     assert body == json_admin
+
+def test_patch_admin(test_app, requests_mock):
+    admin_id = 1
+    token = "a token"
+    path_client = "/admins/" + str(admin_id)
+    data_client = { "token": token }
+
+    new_token = 'a new token'
+    url_admin = USERS_BACKEND_URL + "/admins/" + str(admin_id)
+    json_admin = { "token": new_token }
+    requests_mock.patch(url_admin, status_code=200, json=json_admin)
+
+    client = test_app.test_client()
+    response = client.patch(path_client, data=json.dumps(data_client), content_type='application/json')
+    assert response.status_code == 200
+    body = json.loads(response.data.decode())
+    assert body == json_admin
